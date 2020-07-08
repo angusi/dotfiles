f69e9376e41040e1cce335d29c4630967844157b
Angus Ireland <github-dotfiles@angusi.net>
2020-07-08 10:30:33 +0100

    Add drop_cache alias to rescue WSL2 memory/cache problems

--- wsl/alias.sh
+++ wsl/alias.sh
@@ -5,3 +5,5 @@ alias ssh="ssh -F ~/.sshconfig"
 alias scp="scp -F ~/.sshconfig"
 alias mosh="mosh --ssh \"ssh -F ~/.sshconfig\""
 alias subl="\"/c/Program Files/Sublime Text 3/subl.exe\""
+
+alias drop_cache="sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
