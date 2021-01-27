1b15cec4f7dd7d564908fc99dfb7aa3c3b6203d9
Angus Ireland <github-dotfiles@angusi.net>
2021-01-27 10:09:49 +0000

    Set up native Docker

--- wsl/docker.sh
+++ wsl/docker.sh
@@ -1,7 +1,8 @@
 if [[ "$(hostname -s)" == "PC5"* ]]; then
-    export DOCKER_TLS_VERIFY=""
-    export DOCKER_HOST="tcp://0.0.0.0:2375"
+#    export DOCKER_TLS_VERIFY=""
+#    export DOCKER_HOST="tcp://0.0.0.0:2375"
     #export DOCKER_CERT_PATH="/mnt/c/Users/aai/.docker/machine/machines/default"
     #export DOCKER_MACHINE_NAME="default"
     #export COMPOSE_CONVERT_WINDOWS_PATHS="true"
+    sudo service docker start
 fi
